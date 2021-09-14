549636def8e9e79b8446b52ffe103a46840d6d67
Josh Nolan <nolanjosh1979@gmail.com>
2021-09-14 13:17:06 -0500

    added new projects/added and fixed contact link

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ const options = {
     linkSelector:
         'a[href^="' +
         window.location.origin +
-        '"]:not([data-no-swup]), a[href^="/"]:not([data-no-swup]), a[href^="./"]:not([data-no-swup]), a[href^="#"]:not([data-no-swup])'
+        '"]:not([data-no-swup]), a[href^="/"]:not([data-no-swup]), a[href^="./"]:not([data-no-swup])'
 };
 
 
 const swup = new Swup(options);
 console.log(swup)
+
